85514395c6bab40cc0cefb54d155261985b8d0be
이세준 <gr2nsky@isejun-ui-MacBookPro.local>
2025-01-18 01:21:42 +0900

    feat : accounts_signup

---
+++ accounts/serializers.py
@@ -0,0 +1,16 @@
+from .models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'nickname']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            nickname=validated_data['nickname']
+        )
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework import status
 
-# Create your views here.
+class UserSignupAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            response_data = {
+                "username": user.username,
+                "nickname": user.nickname,
+                "roles": [{"role": user.role}],
+            }
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
