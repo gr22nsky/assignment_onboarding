ae151d7886aceb786c54e97492d123dca6502a93
이세준 <gr2nsky@isejun-ui-MacBookPro.local>
2025-01-18 03:46:49 +0900

    "swagger setting"

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="assignment_onboarding",
+      default_version='v1',
+      description="온보딩 과제 API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
 ]
