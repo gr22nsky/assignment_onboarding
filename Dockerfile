c5d0e589a2b104b12a2a2a0773371669e05fc00d
이세준 <gr2nsky@isejun-ui-MacBookPro.local>
2025-01-18 17:13:16 +0900

    "nginx_issue"

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,18 @@
-# 베이스 이미지 설정
-FROM python:3.9-slim
+# Python 이미지 사용
+FROM python:3.11-slim
 
 # 작업 디렉토리 설정
 WORKDIR /app
 
-# 종속성 복사 및 설치
+# 의존성 설치
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# 프로젝트 파일 복사
+# 애플리케이션 코드 복사
 COPY . .
 
-# 환경 변수 설정
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+# 정적 파일 모으기
+RUN python manage.py collectstatic --noinput
 
-# 애플리케이션 실행
-CMD ["gunicorn", "--bind", "0.0.0.0:8000", "backend.wsgi:application"]
-
+# Gunicorn 실행
+CMD ["gunicorn", "backend.wsgi:application", "--bind", "0.0.0.0:8000"]
--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,6 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     'drf_yasg',
-    'rest_framework_swagger',
     #local apps
     "accounts",
 ]
@@ -128,11 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
